9968e1eed3b125c658e5f58192fea15c9e4287a6
pcrainer38 <pammie_78704@yahoo.com>
2023-10-07 02:36:03 -0500

    added to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 var inputGroupEl = document.querySelector('.input-group')
+var currWeatherEl = document.querySelector('#current-weather');
+var fiveDayEl = document.querySelector('.five-day');
+
 
 var apiKey = 'b25ce75b259c4e61caf9ce00a1f90876';
 
