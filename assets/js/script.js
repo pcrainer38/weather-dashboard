3969c2056fd6a172edd04624ea4405975983751b
pcrainer38 <pammie_78704@yahoo.com>
2023-10-07 17:55:35 -0500

    updated html and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,34 @@
-var inputGroupEl = document.querySelector('.input-group')
+// var inputGroupEl = document.querySelector('.input-group')
 var currWeatherEl = document.querySelector('#current-weather');
 var fiveDayEl = document.querySelector('.five-day');
-
-
+var searchBtnEl = document.querySelector('.btn');
+var cardDataEl = document.querySelector('.card-1');
+ 
+var lat = 30.266666;
+var lon = 97.733330
 var apiKey = 'b25ce75b259c4e61caf9ce00a1f90876';
 
 function handleSearchFormSubmit(event) {
-    event.preventDefault();
+    var searchInputVal = document.querySelector('.input-group').value;
 
-    if (!inputGroupEl) {
+    if (!searchInputVal) {
         console.error('You need a search input value!');
         return;
+    } else {
+        console.log('Great choice!');
     }
-    
+
+    searchApi(searchInputVal);
 }
+
+function searchApi (query) {
+    var wxQueryUrl = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=`;
+    wxQueryUrl = wxQueryUrl.replace("{lat}", lat);
+    wxQueryUrl = wxQueryUrl.replace('{lon}', lon);
+    wxQueryUrl = wxQueryUrl + apiKey;
+    console.log(wxQueryUrl);
+
+}
+
+
+searchBtnEl.addEventListener('click', handleSearchFormSubmit);
