6eed873db1db0f47681e42ab93dbd4777658cbc2
pcrainer38 <pammie_78704@yahoo.com>
2023-10-08 22:25:35 -0500

    changes to html and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
-// var inputGroupEl = document.querySelector('.input-group')
+var dateDisplayEl = document.querySelector('#currentDay');
+var inputGroupEl = document.querySelector('.input-group')
 var currWeatherEl = document.querySelector('#current-weather');
 var fiveDayEl = document.querySelector('.five-day');
 var searchBtnEl = document.querySelector('.btn');
-var cardDataEl = document.querySelector('.card-1');
- 
+
+// Cedar Park
 var lat = 30.266666;
-var lon = 97.733330
+var lon = -97.733330;
 var apiKey = 'b25ce75b259c4e61caf9ce00a1f90876';
+
+function displayDate() {
+    var today = dayjs().format('MMMM D, YYYY');
+    // dateDisplayEl.text(today);
+    console.log(today);
+}
+
 
 function handleSearchFormSubmit(event) {
     var searchInputVal = document.querySelector('.input-group').value;
@@ -23,12 +31,46 @@ function handleSearchFormSubmit(event) {
 
 function searchApi (query) {
     var wxQueryUrl = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=`;
-    wxQueryUrl = wxQueryUrl.replace("{lat}", lat);
+    wxQueryUrl = wxQueryUrl.replace('{lat}', lat);
     wxQueryUrl = wxQueryUrl.replace('{lon}', lon);
     wxQueryUrl = wxQueryUrl + apiKey;
     console.log(wxQueryUrl);
 
+    fetch(wxQueryUrl)
+        .then (function (response) {
+
+            if (!response.ok) {
+                throw response.json();
+            } else {
+
+            return response.json();
+        }
+    })
+        .then(function (data) {
+            currWeatherEl.textContent = data.search.query;
+
+            console.log(data);
+
+            if (!data.results.length) {
+                console.log('No results found!');
+                currWeatherEl.innerHTML = '<h3>No results found, search again!</h3>';
+            } else {
+                currWeatherEl.textContent = '';
+                for (var i = 0; i < data.results.length; i++) {
+                    printResults(data.results[i]);
+                }
+            }
+        })
+        .catch(function (error) {
+            //  console.error(error);
+        });
+        
 }
 
 
+
+
 searchBtnEl.addEventListener('click', handleSearchFormSubmit);
+displayDate();
+
+
